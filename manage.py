6b44b811e79b0e5290d84ec76fc76b1d4d7ff828
linche <linche0502@yahoo.com.tw>
2023-09-07 14:44:23 +0800

    modify

--- manage.py
+++ manage.py
@@ -17,8 +17,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # python manage.py startapp chat
     # execute_from_command_line(sys.argv)
+    
     execute_from_command_line(['manage.py', 'runserver', '127.0.0.1:8080'])
+    
     # execute_from_command_line(['manage.py', 'makemigrations'])
     # execute_from_command_line(['manage.py', 'migrate'])
 
--- test2.py
+++ test2.py
@@ -3,22 +3,53 @@ import openai
 
 
 
-openai.api_key= "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"
+# openai.api_key= ""
+# 
+# dialogue= []
+# while True:
+#     text= input()
+#     if not text:
+#         break
+#     dialogue.append({"role": "user", "content": f"{text}"})
+#     response= openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages= dialogue
+#     )
+#     
+#     try:
+#         print(response['choices'][0]['message']['content'])
+#     except Exception as e:
+#         print(e)
+#         print(response)
+#     print('\n\n\n')
 
-dialogue= []
+
+import requests
+from bardapi import Bard
+token= ""
+
+session = requests.Session()
+session.headers = {
+            "Host": "bard.google.com",
+            "X-Same-Domain": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "Origin": "https://bard.google.com",
+            "Referer": "https://bard.google.com/",
+        }
+session.cookies.set("__Secure-1PSID", token)
+bard= Bard(token=token,session=session, timeout=30)
+
 while True:
     text= input()
-    if text in ['','0']:
+    if not text:
         break
-    dialogue.append({"role": "user", "content": f"{text}"})
-    response= openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages= dialogue
-    )
-    
-    try:
-        print(response['choices'][0]['message']['content'])
-    except Exception as e:
-        print(e)
-        print(response)
-    print('\n\n\n')
+    print(bard.get_answer(text)['content'])
+
+
+
+
+
+
+
+
