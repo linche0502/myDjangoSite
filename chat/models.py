ca49970fdb853b5b1be2edc06a919473ac2b40b4
linche <linche0502@yahoo.com.tw>
2023-09-08 17:30:00 +0800

    m

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import sqlite3
 
 # Create your models here.
+
+
+
+# orm架構
+class Users(models.Model):
+    
+    name= models.TextField(default='')
+    msg= models.TextField(default='')
+    sendTime= models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = "Users"
+
+
+# SQL語法
+def insert():
+    conn = sqlite3.connect("db.sqlite3")
+    cursor = conn.cursor()
+    cursor
